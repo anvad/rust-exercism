13f3759303b2f2ad2d2b2238fa93b8bbe8da6a50
Anil Vadlapatla <anilv@live.com>
2023-01-11 14:57:55 -0800

    binary-search  - basic working version for generic types

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,7 +1,23 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+pub fn find<T>(array: &[T], key: T) -> Option<usize>
+where
+    T: PartialOrd,
+{
+    find_inner(&array, key, 0, array.len())
+}
+
+fn find_inner<T>(array: &[T], key: T, start: usize, end: usize) -> Option<usize>
+where
+    T: PartialOrd,
+{
+    if start == end {
+        return None;
+    }
+    let mid = start + (end - start) / 2;
+    if key == array[mid] {
+        return Some(mid);
+    } else if key < array[mid] {
+        return find_inner(&array, key, start, mid);
+    } else {
+        return find_inner(&array, key, mid + 1, end);
+    }
 }
