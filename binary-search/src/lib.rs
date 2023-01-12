ec2171c653f3629b8f1431e836058a69e9acf4d9
Anil Vadlapatla <anilv@live.com>
2023-01-12 07:12:39 -0800

    binary-search - denenr's solution

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,26 +1,13 @@
-pub fn find<S, T>(array: S, key: T) -> Option<usize>
-where
-    T: PartialOrd,
-    S: AsRef<[T]>,
-{
-    find_inner(&array, key, 0, array.as_ref().len())
-}
-
-fn find_inner<S, T>(array: S, key: T, start: usize, end: usize) -> Option<usize>
-where
-    T: PartialOrd,
-    S: AsRef<[T]>,
-{
-    if start == end {
-        return None;
-    }
+// denenr's solution is good. Replicating it below
+// my copy of his solution initially had a `array.len() == 0` check
+//  then, looking back at his solution, i saw how `.get()` returns an Option
+//  and we can use `?` to short circuit return
+pub fn find<S: AsRef<[T]>, T: Ord>(array: S, key: T) -> Option<usize> {
     let array = array.as_ref();
-    let mid = start + (end - start) / 2;
-    if key == array[mid] {
-        return Some(mid);
-    } else if key < array[mid] {
-        return find_inner(array, key, start, mid);
-    } else {
-        return find_inner(array, key, mid + 1, end);
+    let mid = array.len() / 2;
+    match key.cmp(array.get(mid)?) {
+        std::cmp::Ordering::Equal => Some(mid),
+        std::cmp::Ordering::Less => find(&array[..mid], key),
+        std::cmp::Ordering::Greater => find(&array[mid + 1..], key).map(|i| mid + 1 + i),
     }
 }
