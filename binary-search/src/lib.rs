1724e408778f5695048398330655a6b7fc9d58b1
Anil Vadlapatla <anilv@live.com>
2023-01-11 16:12:03 -0800

    binary-search - generics

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,23 +1,26 @@
-pub fn find<T>(array: &[T], key: T) -> Option<usize>
+pub fn find<S, T>(array: S, key: T) -> Option<usize>
 where
     T: PartialOrd,
+    S: AsRef<[T]>,
 {
-    find_inner(&array, key, 0, array.len())
+    find_inner(&array, key, 0, array.as_ref().len())
 }
 
-fn find_inner<T>(array: &[T], key: T, start: usize, end: usize) -> Option<usize>
+fn find_inner<S, T>(array: S, key: T, start: usize, end: usize) -> Option<usize>
 where
     T: PartialOrd,
+    S: AsRef<[T]>,
 {
     if start == end {
         return None;
     }
+    let array = array.as_ref();
     let mid = start + (end - start) / 2;
     if key == array[mid] {
         return Some(mid);
     } else if key < array[mid] {
-        return find_inner(&array, key, start, mid);
+        return find_inner(array, key, start, mid);
     } else {
-        return find_inner(&array, key, mid + 1, end);
+        return find_inner(array, key, mid + 1, end);
     }
 }
