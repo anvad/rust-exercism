387301a87ae47aeeb8eb27840bb21016ae97a2f8
Anil Vadlapatla <anilv@live.com>
2023-01-13 09:28:22 -0800

    bowling - potatosalad's solution   - added more comments   - compressed code a bit

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -7,9 +7,10 @@ pub enum Error {
 // potatosalad's solution is so neat! Copying his solution below.
 // His basic idea (store the rolls) is similar to mine.
 // Where he saved a lot of complexity is by simply storing numbers.
-// Since i stored enums, i had to spend a lot of code, trying to extract the numbers.
+// Since i originally stored enums, i had to spend a lot of code, trying to extract the numbers.
 // Another clever idea he used is to use the `score` function to determine game end!
-// Finally, i learnt something new about rust- using Default to create a new instance of a struct.
+// I learnt something new about rust- using Default to create a new instance of a struct.
+// I used `?` to short-circuit `score()` when getting optional values.
 #[derive(Debug, Default)]
 pub struct BowlingGame {
     rolls: Vec<u16>, // list of pins that fell on each roll played so far
@@ -28,36 +29,32 @@ impl BowlingGame {
         if pins > 10 || (self.second && self.rolls.last().unwrap() + pins > 10) {
             return Err(Error::NotEnoughPinsLeft);
         }
-        // if this roll is a strike, then next roll is a first (i.e. not second)
-        // else, next roll toggles between first and second
-        if pins == 10 {
-            self.second = false;
-        } else {
-            self.second = !self.second;
-        }
         self.rolls.push(pins);
+        // if this roll is a strike, or this roll is the second, then next roll is the first
+        self.second = !(pins == 10 || self.second);
         Ok(())
     }
 
     pub fn score(&self) -> Option<u16> {
         // make sure 10 frames are rolled, and any bonus rolls after that.
+        // for scoring, every "frame" needs at least two rolls. For strike, we need three.
         let mut sum = 0;
-        let mut i = 0;
+        let mut roll_index = 0;
         for _ in 0..10 {
-            let first = *self.rolls.get(i)?;
+            let first = *self.rolls.get(roll_index)?;
+            let second_or_bonus1 = *self.rolls.get(roll_index + 1)?;
+            sum += first + second_or_bonus1;
             if first == 10 {
-                let (bonus1, bonus2) = (*self.rolls.get(i + 1)?, *self.rolls.get(i + 2)?);
-                sum += first + bonus1 + bonus2;
-                i += 1;
+                let bonus2 = *self.rolls.get(roll_index + 2)?;
+                sum += bonus2;
+                roll_index += 1; // only for strike, we advance frame by one roll
                 continue;
             }
-            let second = *self.rolls.get(i + 1)?;
-            sum += first + second;
-            if first + second == 10 {
-                let bonus1 = *self.rolls.get(i + 2)?;
+            if first + second_or_bonus1 == 10 {
+                let bonus1 = *self.rolls.get(roll_index + 2)?;
                 sum += bonus1;
             }
-            i += 2;
+            roll_index += 2; // we advance frame by two rolls
         }
         Some(sum)
     }
