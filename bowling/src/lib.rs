3d026c28c9b20a01ddf7475943937243d3648571
Anil Vadlapatla <anilv@live.com>
2023-01-13 09:08:07 -0800

    bowling - potatosalad's solution

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -4,143 +4,61 @@ pub enum Error {
     GameComplete,
 }
 
-// all we need to store is the sequence of pins rolled
-//  and number of frames, score in each frame, whether game is done can
-//  be calculated from that
-
-#[derive(Debug)]
+// potatosalad's solution is so neat! Copying his solution below.
+// His basic idea (store the rolls) is similar to mine.
+// Where he saved a lot of complexity is by simply storing numbers.
+// Since i stored enums, i had to spend a lot of code, trying to extract the numbers.
+// Another clever idea he used is to use the `score` function to determine game end!
+// Finally, i learnt something new about rust- using Default to create a new instance of a struct.
+#[derive(Debug, Default)]
 pub struct BowlingGame {
-    rolls: Vec<Roll>, // list of all rolls played so far
-    rolls_left: u16,  // max. number of possible rolls left at any point
-    pins_left: u16,   // pins remaining after last roll, range is 1..=10
+    rolls: Vec<u16>, // list of pins that fell on each roll played so far
+    second: bool, // whether next roll will be the 2nd roll (pins are not reset before the 2nd roll)
 }
-
-// the number stores the number of pins that fell in this roll
-#[derive(PartialEq, Debug)]
-enum Roll {
-    First(u16), // first roll, so frame is not done yet
-    Open(u16),  // after 2nd roll, if pins are still standing, then we have an open frame
-    Strike,     // frame is done after just one roll!
-    Spare(u16), // frame is complete, but needed two rolls
-    Bonus(u16), // Bonus ball after all frames are done
-}
-
-use Roll::*;
 
 impl BowlingGame {
     pub fn new() -> Self {
-        Self {
-            rolls: vec![],
-            rolls_left: 20,
-            pins_left: 10,
-        }
-    }
-
-    fn is_game_done(&self) -> bool {
-        self.rolls_left < 1
-    }
-
-    /// `rolls_diff` is how many roll count to subtract from `self.rolls_left`
-    /// `pins` is how many pins to remove from the lane (i.e. `self.pins_left`)
-    fn append_roll(&mut self, roll: Roll, rolls_diff: u16, pins: u16) -> Result<(), Error> {
-        self.pins_left = if self.pins_left == pins {
-            10 // reset frame
-        } else {
-            self.pins_left - pins
-        };
-        self.rolls_left -= rolls_diff;
-        self.rolls.push(roll);
-        Ok(())
-    }
-
-    /// returns the number of completed frames
-    fn frames(&self) -> u8 {
-        self.rolls
-            .iter()
-            .map(|r| match *r {
-                Roll::First(_) => 0,
-                Roll::Bonus(_) => 0,
-                _ => 1,
-            })
-            .sum()
-    }
-    fn validate_roll(&self, pins: u16) -> Result<(), Error> {
-        if self.is_game_done() {
-            Err(Error::GameComplete)
-        } else if pins > self.pins_left {
-            Err(Error::NotEnoughPinsLeft)
-        } else {
-            Ok(())
-        }
-    }
-    fn spare_in_last_frame(&self, pins: u16) -> bool {
-        let frames_played = self.frames();
-        pins == self.pins_left && frames_played == 9
-    }
-    fn strike_in_last_frame(&self, pins: u16) -> bool {
-        let frames_played = self.frames();
-        pins == 10 && frames_played == 9
-    }
-    fn bonus_roll(&self) -> bool {
-        let frames_played = self.frames();
-        frames_played == 10
-    }
-    fn strike(&self, pins: u16) -> bool {
-        pins == 10
-    }
-    fn spare(&self, pins: u16) -> bool {
-        pins == self.pins_left
+        Default::default()
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        self.validate_roll(pins)?;
-
-        match self.rolls.last() {
-            Some(First(_)) if self.spare_in_last_frame(pins) => {
-                self.append_roll(Spare(pins), 0, pins)
-            }
-            Some(First(_)) if self.spare(pins) => self.append_roll(Spare(pins), 1, pins),
-            Some(First(_)) => self.append_roll(Open(pins), 1, self.pins_left),
-
-            // None or Spare or Open or Strike => Strike or First() or Bonus()
-            _ if self.bonus_roll() => self.append_roll(Bonus(pins), 1, pins),
-            _ if self.strike_in_last_frame(pins) => self.append_roll(Strike, 0, pins),
-            _ if self.strike(pins) => self.append_roll(Strike, 2, pins),
-            _ => self.append_roll(First(pins), 1, pins),
+        if self.score().is_some() {
+            return Err(Error::GameComplete);
         }
-    }
-
-    /// returns points accrued for each roll, including bonus points
-    fn roll_points(&self, i: usize) -> u16 {
-        let roll = &self.rolls.get(i);
-        let bonus1 = &self.rolls.get(i + 1);
-        let bonus2 = &self.rolls.get(i + 2);
-        match (*roll, *bonus1, *bonus2) {
-            (Some(Strike), Some(f1), Some(f2)) => 10 + roll_pins(f1) + roll_pins(f2),
-            (Some(Spare(p)), Some(f1), _) => *p + roll_pins(f1),
-            (Some(Open(p)), ..) => *p,
-            (Some(First(p)), ..) => *p,
-            _ => 0,
+        if pins > 10 || (self.second && self.rolls.last().unwrap() + pins > 10) {
+            return Err(Error::NotEnoughPinsLeft);
         }
+        // if this roll is a strike, then next roll is a first (i.e. not second)
+        // else, next roll toggles between first and second
+        if pins == 10 {
+            self.second = false;
+        } else {
+            self.second = !self.second;
+        }
+        self.rolls.push(pins);
+        Ok(())
     }
 
     pub fn score(&self) -> Option<u16> {
-        if self.is_game_done() {
-            let sum = (0..self.rolls.len()).map(|i| self.roll_points(i)).sum();
-            Some(sum)
-        } else {
-            None
+        // make sure 10 frames are rolled, and any bonus rolls after that.
+        let mut sum = 0;
+        let mut i = 0;
+        for _ in 0..10 {
+            let first = *self.rolls.get(i)?;
+            if first == 10 {
+                let (bonus1, bonus2) = (*self.rolls.get(i + 1)?, *self.rolls.get(i + 2)?);
+                sum += first + bonus1 + bonus2;
+                i += 1;
+                continue;
+            }
+            let second = *self.rolls.get(i + 1)?;
+            sum += first + second;
+            if first + second == 10 {
+                let bonus1 = *self.rolls.get(i + 2)?;
+                sum += bonus1;
+            }
+            i += 2;
         }
-    }
-}
-
-// given a &Roll, returns the number of pins that fell
-fn roll_pins(roll: &Roll) -> u16 {
-    match *roll {
-        First(p) => p,
-        Open(p) => p,
-        Spare(p) => p,
-        Strike => 10,
-        Bonus(p) => p,
+        Some(sum)
     }
 }
