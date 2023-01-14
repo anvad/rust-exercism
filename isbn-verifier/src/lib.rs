0a1d69049a7f08ba83d48aeebd6c4c20ee915a65
Anil Vadlapatla <anilv@live.com>
2023-01-14 11:35:40 -0800

    isbn-verifier - abstracted checks, gave meaningful names

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -1,11 +1,11 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
+    let is_digit = |ch: &char| *ch >= '0' && *ch <= '9';
+    let last_char = |s: &[char]| s.last().unwrap().clone();
     let isbn: Vec<char> = isbn.chars().filter(|ch| *ch != '-').collect();
     isbn.len() == 10
-        && isbn[..isbn.len() - 1]
-            .iter()
-            .all(|ch| *ch >= '0' && *ch <= '9')
-        && (*isbn.last().unwrap() == 'X' || isbn.last().unwrap().to_digit(10).is_some())
+        && isbn[..isbn.len() - 1].iter().all(is_digit)
+        && (last_char(&isbn) == 'X' || is_digit(&last_char(&isbn)))
         && isbn
             .iter()
             .enumerate()
