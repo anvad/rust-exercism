28a24e5fef0676987be5764665f989ce52a823ea
Anil Vadlapatla <anilv@live.com>
2023-01-14 11:19:01 -0800

    isbn-verifier - basic passing

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -1,4 +1,19 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    unimplemented!("Is {:?} a valid ISBN number?", isbn);
+    let isbn: Vec<char> = isbn.chars().filter(|ch| *ch != '-').collect();
+    isbn.len() == 10
+        && isbn[..isbn.len() - 1]
+            .iter()
+            .all(|ch| *ch >= '0' && *ch <= '9')
+        && (*isbn.last().unwrap() == 'X' || isbn.last().unwrap().to_digit(10).is_some())
+        && isbn
+            .iter()
+            .enumerate()
+            .map(|(i, ch)| match ch {
+                'X' => 10 * (10 - i as u32),
+                _ => ch.to_digit(10).unwrap() * (10 - i as u32),
+            })
+            .sum::<u32>()
+            % 11
+            == 0
 }
