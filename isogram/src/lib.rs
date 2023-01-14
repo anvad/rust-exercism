66fcce8b8f4f7870e9f426af5aaf6799619a738a
Anil Vadlapatla <anilv@live.com>
2023-01-14 14:42:14 -0800

    isogram - tests passing

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -1,3 +1,18 @@
+use std::collections::HashMap;
+
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    match candidate
+        .chars()
+        .filter(|&ch| ch != '-' && ch != ' ')
+        .map(|ch| ch.to_lowercase().to_string())
+        .try_fold(HashMap::new(), |mut map, ch| match map.get(&ch) {
+            Some(_) => None,
+            _ => {
+                map.insert(ch, 1);
+                Some(map)
+            }
+        }) {
+        Some(_) => true,
+        _ => false,
+    }
 }
