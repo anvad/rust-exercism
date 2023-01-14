3ff0a009169c6fa20b9e180f225173b2fff4faee
Anil Vadlapatla <anilv@live.com>
2023-01-14 15:46:56 -0800

    isogram - rsalmei's solution

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -1,17 +1,12 @@
-// looked at hansrodtang's solution elegant solution.
-// in there, i found rsalmei's comment about using u32 to map the 26 chars of ascii alphabet
-//  i still think hansrodtang's solution is more readable and
-//  more general since it can handle most non-ascii chars too,
-//  whereas this solution (developed from rsalmei's hints) only works for ascii strings.
+// copied rsalmei's solution
+//  learnt that bool has a `.then` method to convert bool to Option
+//  and realized how i could have just used `is_some()` instead of matching on `Some`!
 pub fn check(candidate: &str) -> bool {
-    match candidate
+    candidate
         .chars()
         .filter(|&ch| ch != '-' && ch != ' ')
         .flat_map(|ch| ch.to_lowercase())
-        .map(|ch| 2u32.pow(((ch as u8) - b'a') as u32))
-        .try_fold(0, |sum, n| if sum & n == 0 { Some(sum + n) } else { None })
-    {
-        Some(_) => true,
-        _ => false,
-    }
+        .map(|ch| 1u32 << (ch as u8) - b'a')
+        .try_fold(0, |sum, n| (sum & n == 0).then(|| sum + n))
+        .is_some()
 }
