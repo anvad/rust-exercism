59b9cfaabe1a013ffcecae0224155da62aa1f4b9
Anil Vadlapatla <anilv@live.com>
2023-01-16 14:06:07 -0800

    pangram - tests passing

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,4 +1,16 @@
+use std::collections::HashSet;
+
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {} a pangram?", sentence);
+    let mut alphabet = HashSet::<char>::new();
+    for ch in sentence
+        .bytes()
+        .filter(|ch| (*ch as char).is_ascii_alphabetic())
+        .map(|ch| (ch as char).to_ascii_lowercase())
+    {
+        if alphabet.insert(ch) && alphabet.len() == 26 {
+            return true;
+        }
+    }
+    false
 }
