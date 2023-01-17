b1e88fdec8ae9c7d19c6b8c26309821bf57048d2
Anil Vadlapatla <anilv@live.com>
2023-01-17 09:37:40 -0800

    pascals-triangle - compacted code with `match`  - added comments

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -6,29 +6,29 @@ impl PascalsTriangle {
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        let mut pascals: Vec<Vec<u32>> = vec![];
-        if self.0 == 0 {
-            return pascals;
-        }
-        pascals.push(vec![1]);
-        if self.0 == 1 {
-            return pascals;
-        }
-        pascals.push(vec![1, 1]);
-        if self.0 == 2 {
-            return pascals;
-        }
-        let max_i = self.0 as usize;
-        for i in 2..max_i {
-            let prev_row = &pascals[i - 1];
-            let mut row = vec![1; i + 1];
-            let max = i / 2;
-            for j in 1..=max {
-                row[j] = prev_row[j - 1] + prev_row[j];
-                row[i - j] = row[j];
+        // we hardcode the first two rows to avoid checking for underflow of u32 in each loop
+        match self.0 {
+            0 => vec![],
+            1 => vec![vec![1]],
+            2 => vec![vec![1], vec![1, 1]],
+            _ => {
+                let mut pascals: Vec<Vec<u32>> = vec![vec![1], vec![1, 1]];
+                pascals.reserve(self.0 as usize - 2); // minimize re-sizing
+                let max_i = self.0 as usize;
+                for i in 2..max_i {
+                    let prev_row = &pascals[i - 1];
+                    let mut row = vec![1; i + 1];
+
+                    // since triangle is symmetric, we only need to loop till half the row
+                    let max_j = i / 2;
+                    for j in 1..=max_j {
+                        row[j] = prev_row[j - 1] + prev_row[j];
+                        row[i - j] = row[j];
+                    }
+                    pascals.push(row);
+                }
+                pascals
             }
-            pascals.push(row);
         }
-        pascals
     }
 }
