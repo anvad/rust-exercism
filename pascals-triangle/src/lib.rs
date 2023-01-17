3c4e1a2447f39600b6d46fde1e48b1cf526d114b
Anil Vadlapatla <anilv@live.com>
2023-01-17 09:26:14 -0800

    pascals-triangle - tests passing

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -1,11 +1,34 @@
-pub struct PascalsTriangle;
+pub struct PascalsTriangle(u32);
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!("create Pascal's triangle with {} rows", row_count);
+        Self(row_count)
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        let mut pascals: Vec<Vec<u32>> = vec![];
+        if self.0 == 0 {
+            return pascals;
+        }
+        pascals.push(vec![1]);
+        if self.0 == 1 {
+            return pascals;
+        }
+        pascals.push(vec![1, 1]);
+        if self.0 == 2 {
+            return pascals;
+        }
+        let max_i = self.0 as usize;
+        for i in 2..max_i {
+            let prev_row = &pascals[i - 1];
+            let mut row = vec![1; i + 1];
+            let max = i / 2;
+            for j in 1..=max {
+                row[j] = prev_row[j - 1] + prev_row[j];
+                row[i - j] = row[j];
+            }
+            pascals.push(row);
+        }
+        pascals
     }
 }
